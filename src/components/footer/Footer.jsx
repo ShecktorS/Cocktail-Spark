bb877c95003aa175b3510fd3cec73d6fe368c569
ShecktorS <ettoresanfilippo@gmail.com>
2023-03-20 20:21:39 +0100

    Footer structure completed (no CSS)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,41 @@
 import "./index.scss";
 
 const Footer = () => {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <div className="Footer__bridge">
+        <h2>Bridge</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          corporis laboriosam magni rerum?
+        </p>
+        <div>
+          <p> 🕘 Lun-Dom: 09:00 - 21:00</p>
+          <p> 🚩 Via dei Malapigghiati, 35, Bagheria </p>
+          <p> 📞 +39 3333333565</p>
+        </div>
+      </div>
+      <div className="Footer__pages">
+        <h3>Pagine</h3>
+        <p>Home</p>
+        <p>Prenotazioni</p>
+        <p>Menu</p>
+        <p>Contatti</p>
+      </div>
+      <div className="Footer__openingHours">
+        <h3>Orari di Apertura</h3>
+        <div>
+          <p>Lunedì-Venerdì:</p>
+          <p>10:00-01:00</p>
+        </div>
+        <div>
+          <p>Sabato-Domenica:</p>
+          <p>10:00-03:00</p>
+        </div>
+      </div>
+      <h3 className="Footer__instagram">Instagram</h3>
+    </div>
+  );
 };
 
 export default Footer;
